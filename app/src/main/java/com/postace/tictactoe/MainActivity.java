2e2c0d26d56b29844f594c2ebff5e00ea03de448
Hai <thanhhai.fu@gmail.com>
2016-06-28 17:53:18 +0700

    Fixing winning state
    The diagonal from top-left to bottom-right must be {0,4,8} instead of {0,4,7}

--- app/src/main/java/com/postace/tictactoe/MainActivity.java
+++ app/src/main/java/com/postace/tictactoe/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     boolean gameOver = false;       // game over when full board or some one won
     int[] boardState = {0, 0, 0, 0, 0, 0, 0, 0, 0};
     int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
-            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 7}, {2, 4, 6}};
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
